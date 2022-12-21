51e855a6dc1bdd7ed683b0ae41e8490eb185e018
Roniathayde <pontes014@gmail.com>
2022-12-21 15:42:17 -0300

    adiciona links vinculados as sessões do projeto

---
+++ assets/js/birthday.js
@@ -0,0 +1,11 @@
+let date = document.querySelector('.about_desc--content');
+
+let myBirthday = new Date('05, 08, 2002');
+
+let toDay = new Date();
+
+
+console.log(toDay - myBirthday)
+
+
+console.log(date.textContent.split(' ')[4]);
